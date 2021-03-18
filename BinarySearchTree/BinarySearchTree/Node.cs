1fe3da5c03850c8d7fa112db0169e3837c92ff1d
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-18 17:02:27 -0500

    Insert method working

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        string SData { get; set; } // represents name
+        int IData { get; set; }    // represents place on top ten list
+        Node Left { get; set; }    // left child (left subtree)
+        Node Right { get; set; }   // right child (right subtree)
+
+        public Node() { }
+
+        public Node(string sData, int iData)
+        {
+            SData = sData;
+            IData = iData;
+        }
+
+        public void Insert(int position, string name)
+        {
+            if(position <= IData) //If position is less than or equal to IData (existing position), the new node will go on the left
+            {
+                if(Left == null) //If the left node is null
+                {
+                    Left = new Node(name, position); //Assign a node to Left using the provided position and name
+                }
+                else //If the left node is NOT null
+                {
+                    Left.Insert(position, name);
+                }
+            }
+            else //Otherwise, the new node will go on the right
+            {
+                if (Right == null)
+                {
+                    Right = new Node(name, position);
+                }
+                else
+                {
+                    Right.Insert(position, name);
+                }
+            }
+        }
+
+        //public string Search(string name)
+        //{
+        //    if (name == SData)
+        //    {
+        //        return "Name: " + SData + ", Position: " + IData;
+        //    }
+        //    else if (name)
+        //}
+    }
+}
--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -6,7 +6,17 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node node = new Node();
+            node.Insert(5, "William");
+            node.Insert(1, "Noah");
+            node.Insert(10, "Benjamin");
+            node.Insert(8, "Alexander");
+            node.Insert(9, "Michael");
+            node.Insert(2, "Liam");
+            node.Insert(3, "Mason");
+            node.Insert(7, "James");
+            node.Insert(6, "Ethan");
+            node.Insert(4, "Jacob");
         }
     }
 }
